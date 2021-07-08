f321254e86d078c8836bf6b99c7dc9d38eed539e
~Nebula~ <stefanb1289@gmail.com>
2021-07-08 16:56:07 +0100

    Fixed audio resampling stuff

--- taiko-rs-client/src/game/audio/mod.rs
+++ taiko-rs-client/src/game/audio/mod.rs
@@ -3,6 +3,7 @@ use cpal::{
     traits::{HostTrait, DeviceTrait, StreamTrait}
 };
 
+use rubato::{SincFixedIn, Resampler, InterpolationParameters, InterpolationType, WindowFunction};
 use symphonia::core::{
     audio::{AudioBufferRef, Signal},
     io::MediaSourceStream,
@@ -12,8 +13,6 @@ use symphonia::core::{
 use std::{rc::Rc, sync::atomic::{AtomicUsize, Ordering}};
 use std::sync::Arc;
 use parking_lot::Mutex;
-
-use itertools::Itertools;
 
 const SOUND_LIST:[&'static str; 4] = [
     "audio/don.wav",
@@ -32,6 +31,25 @@ pub struct Audio {
 impl Audio {
     // todo: fix everything so nothing crashes and you can always change the device later etc
     pub fn setup() -> cpal::Stream {
+        let host = cpal::default_host();
+
+        let device = host.default_output_device()
+            .expect("No default output device available.");
+
+        let mut supported_configs = device.supported_output_configs()
+            .expect("Error while querying configs.");
+
+        let supported_config_range = supported_configs.next()
+            .expect("No supported config?");
+
+        println!("Range Rate: {}-{}Hz", supported_config_range.min_sample_rate().0, supported_config_range.max_sample_rate().0);
+
+        let supported_config = supported_config_range.with_max_sample_rate();
+
+        let stream_rate = supported_config.sample_rate().0;
+
+        println!("Sample Rate Stream: {}", stream_rate);
+
         // temp
         let file = std::fs::File::open("audio/time-traveler.mp3")
         .expect("Failed to open file.");
@@ -60,21 +78,30 @@ impl Audio {
         .expect("Failed to get codecs and produce decoder.");
 
         let mut samples = Vec::new();
-        let mut rate = 0;
+        let mut track_rate = 0;
+        let mut channels = 0;
 
         while let Ok(packet) = reader.next_packet() {
             if packet.track_id() != track_id { continue; }
 
             match decoder.decode(&packet) {
                 Ok(AudioBufferRef::F32(f)) => {
-                    if rate == 0 { rate = f.spec().rate };
+                    if track_rate == 0 { track_rate = f.spec().rate; }
+                    if channels == 0 { 
+                        channels = f.spec().channels.count();
 
-                    let channel_0 = f.chan(0).into_iter().cloned();
-                    let channel_1 = f.chan(1).into_iter().cloned();
-                    samples.append(&mut channel_0.interleave(channel_1).collect_vec());
+                        // Populate channels vec
+                        for _ in 0..channels {
+                            samples.push(Vec::new());
+                        }
+                    }
+
+                    for chan in 0..channels {
+                        samples[chan].append(&mut f.chan(chan).to_vec());
+                    }
                 }
 
-                Ok(AudioBufferRef::S32(f)) => {
+                Ok(AudioBufferRef::S32(_f)) => {
                     println!("wat, s32");
                 }
 
@@ -86,54 +113,36 @@ impl Audio {
             }
         }
 
-        let host = cpal::default_host();
+        // todo: Potentially mess with these, shove them in a lazy_static?
+        let params = InterpolationParameters {
+            sinc_len: 256,
+            f_cutoff: 0.95,
+            oversampling_factor: 128,
+            interpolation: InterpolationType::Cubic,
+            window: WindowFunction::Blackman
+        };
 
-        let device = host.default_output_device()
-            .expect("No default output device available.");
+        let mut resampler = SincFixedIn::new(
+            stream_rate as f64 / track_rate as f64,
+            params,
+            samples[0].len(),
+            channels
+        );
 
-        let mut supported_configs = device.supported_output_configs()
-            .expect("Error while querying configs.");
+        let resampled_audio = resampler.process(samples.as_slice())
+        .expect("Failed to resample audio.");
 
-        let supported_config_range = supported_configs.next()
-            .expect("No supported config?");
+        let mut interleaved_samples = Vec::new();
 
-        println!("Range Rate: {}-{}Hz", supported_config_range.min_sample_rate().0, supported_config_range.max_sample_rate().0);
-
-        let supported_config = supported_config_range.with_max_sample_rate();
-
-        let stream_rate = supported_config.sample_rate().0;
-
-        println!("Sample Rate Stream: {}", stream_rate);
-
-        let scale_factor = stream_rate as f32 / rate as f32;
-
-        let mut scaled_samples = vec![0.0; (samples.len() as f32 * scale_factor + 1.0).ceil() as usize];
-
-        // no, its all fucked idk
-        for (old_index, sample) in samples.iter().enumerate() {
-            let sd_len = samples.len();
-
-            let new_index = old_index as f32 * scale_factor;
-            let j = new_index.floor() as usize;
-            let f = new_index.fract();
-
-            let xm1 = samples[(old_index - 2) % sd_len];
-            let x0  = samples[old_index       % sd_len];
-            let x1  = samples[(old_index + 2) % sd_len];
-            let x2  = samples[(old_index + 4) % sd_len];
-
-            let c     = (x1 - xm1) * 0.5;
-            let v     = x0 - x1;
-            let w     = c + v;
-            let a     = w + v + (x2 - x0) * 0.5;
-            let b_neg = w + a;
-
-            scaled_samples[j] = (((a * f) - b_neg) * f + c) * f + x0;
+        for i in 0..samples[0].len() {
+            for chan in 0..channels {
+                interleaved_samples.push(resampled_audio[chan].get(i).map(|&x| x).unwrap_or(0.0));
+            }
         }
 
         let count = AtomicUsize::new(0);
 
-        const volume: f32 = 0.3;
+        const volume: f32 = 0.5;
 
         let stream = device.build_output_stream(
             &supported_config.into(),
@@ -143,10 +152,10 @@ impl Audio {
                 let index = count.load(Ordering::SeqCst);
 
                 for (i, sample) in data.iter_mut().enumerate() {
-                    *sample = volume * scaled_samples[(index + i) % scaled_samples.len()];
+                    *sample = volume * interleaved_samples[(index + i) % interleaved_samples.len()];
                 }
 
-                count.store((index + data.len()) % scaled_samples.len(), Ordering::SeqCst);
+                count.store((index + data.len()) % interleaved_samples.len(), Ordering::SeqCst);
             },
             move |err| {
                 println!("wat: {:?}", err);
