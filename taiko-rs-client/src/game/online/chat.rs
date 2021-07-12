524788315146fb5f4e07586e08fc719aeaee1aa0
Eve <hi.its.eve@gmail.com>
2021-07-12 10:58:12 -0400

    add channel to ChatMessage (might be important)

--- taiko-rs-client/src/game/online/chat.rs
+++ taiko-rs-client/src/game/online/chat.rs
@@ -4,10 +4,10 @@ pub struct Chat {
     messages: Vec<ChatMessage>
 }
 
-
-
 pub struct ChatMessage {
     sender: String,
+    // channel or username
+    channel: String, 
     sender_id: u32,
     timestamp: u64, //TODO: make this not shit
     text: String
