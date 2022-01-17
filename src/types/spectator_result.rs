6a74eeb8226936938d9a4bfb772187da406cfd1c
Eve <hi.its.eve@gmail.com>
2022-01-17 18:27:27 -0500

    add error for speccing yourself, and comments for other enums

--- src/types/spectator_result.rs
+++ src/types/spectator_result.rs
@@ -4,15 +4,23 @@ use crate::prelude::*;
 #[derive(PacketSerialization, Copy, Clone, Debug)]
 #[Packet(type="u8")]
 pub enum SpectateResult {
+    /// spectate request was accepted
     #[Packet(id=0)]
     Ok,
 
+    /// trying to spectate a bot
     #[Packet(id=1)]
     Error_SpectatingBot,
 
+    /// user you're trying to spec doesnt exist or is offline
     #[Packet(id=2)]
     Error_HostOffline,
 
+    /// you're trying to spectate yourself
+    #[Packet(id=3)]
+    Error_SpectatingYourself,
+
+    /// some other error
     #[Packet(id=255, default_variant)]
     Error_Unknown,
 }
