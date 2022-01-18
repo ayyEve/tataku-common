2f54538e869e955792f780d81144cc5c612d8333
Eve <hi.its.eve@gmail.com>
2022-01-18 11:35:35 -0500

    try forcing seaorm to use underscores

--- src/tables/scores_table.rs
+++ src/tables/scores_table.rs
@@ -10,12 +10,19 @@ pub struct Model {
     pub playmode: i16,
     pub score: i64,
     pub combo: i16,
+    #[sea_orm(column_name = "max_combo")]
     pub max_combo: i16,
+    #[sea_orm(column_name = "hit_50")]
     pub hit_50: i16,
+    #[sea_orm(column_name = "hit_100")]
     pub hit_100: i16,
+    #[sea_orm(column_name = "hit_300")]
     pub hit_300: i16,
+    #[sea_orm(column_name = "hit_geki")]
     pub hit_geki: i16,
+    #[sea_orm(column_name = "hit_katu")]
     pub hit_katu: i16,
+    #[sea_orm(column_name = "hit_miss")]
     pub hit_miss: i16,
     pub accuracy: f64,
     pub game: String
