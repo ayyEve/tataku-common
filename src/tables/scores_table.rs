63e0cfbe3ababab6f0759f7bfb80b8f8868a2829
Eve <hi.its.eve@gmail.com>
2022-01-18 11:25:32 -0500

    update scores table to fix score_id column, and add game column

--- src/tables/scores_table.rs
+++ src/tables/scores_table.rs
@@ -6,7 +6,7 @@ pub struct Model {
     pub username: String,
     pub beatmap_hash: String,
     #[sea_orm(primary_key)]
-    pub id: i64,
+    pub score_id: i64,
     pub playmode: i16,
     pub score: i64,
     pub combo: i16,
@@ -17,7 +17,8 @@ pub struct Model {
     pub hit_geki: i16,
     pub hit_katu: i16,
     pub hit_miss: i16,
-    pub accuracy: f64
+    pub accuracy: f64,
+    pub game: String
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
