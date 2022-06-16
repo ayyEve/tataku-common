32c74018683f82decf7d4b57a99d8559f850cb3e
Eve <hi.its.eve@gmail.com>
2022-06-16 17:26:54 -0400

    update scores table

--- src/tables/scores_table.rs
+++ src/tables/scores_table.rs
@@ -6,27 +6,21 @@ pub struct Model {
     pub username: String,
     #[sea_orm(column_name = "user_id")]
     pub user_id: i32,
+
     pub beatmap_hash: String,
+
     #[sea_orm(primary_key)]
     pub score_id: i64,
-    pub playmode: i16,
+
+    #[sea_orm(column_name = "hit_judgments")]
+    pub hit_judgments: String,
     pub score: i64,
     pub combo: i16,
     #[sea_orm(column_name = "max_combo")]
     pub max_combo: i16,
-    #[sea_orm(column_name = "hit_50")]
-    pub hit_50: i16,
-    #[sea_orm(column_name = "hit_100")]
-    pub hit_100: i16,
-    #[sea_orm(column_name = "hit_300")]
-    pub hit_300: i16,
-    #[sea_orm(column_name = "hit_geki")]
-    pub hit_geki: i16,
-    #[sea_orm(column_name = "hit_katu")]
-    pub hit_katu: i16,
-    #[sea_orm(column_name = "hit_miss")]
-    pub hit_miss: i16,
     pub accuracy: f64,
+
+    pub playmode: String,
     pub game: String
 }
 
