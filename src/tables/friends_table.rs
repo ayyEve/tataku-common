55a85ee98c75f8fe8e8a40e5089c8da0a348df86
Eve <hi.its.eve@gmail.com>
2023-02-05 11:38:03 -0500

    more friend helpers

--- src/tables/friends_table.rs
+++ src/tables/friends_table.rs
@@ -20,6 +20,27 @@ impl Model {
         .map(|a|a.friend_id as u32)
         .collect()
     }
+
+    pub async fn remove_friend<'a>(user_id: u32, friend_id: u32, db: &'a DatabaseConnection) {
+        Entity::delete_by_id((user_id as i32, friend_id as i32))
+        .exec(db)
+        .await
+        .unwrap();
+    }
+
+    pub async fn add_friend<'a>(user_id: u32, friend_id: u32, db: &'a DatabaseConnection) {
+        let mut on_conflict = sea_orm::sea_query::OnConflict::new();
+        on_conflict.do_nothing();
+
+        Entity::insert(ActiveModel {
+            user_id: sea_orm::Set(user_id as i32),
+            friend_id: sea_orm::Set(friend_id as i32),
+        })
+        .on_conflict(on_conflict)
+        .exec(db)
+        .await
+        .unwrap();
+    }
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
